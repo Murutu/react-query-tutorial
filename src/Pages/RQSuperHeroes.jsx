46f882a1764d9f6aaf775cd906298d37d7123e99
Murutu <murutupeter@gmail.com>
2023-06-16 14:33:57 +0300

    chore: refetch

--- src/Pages/RQSuperHeroes.jsx
+++ src/Pages/RQSuperHeroes.jsx
@@ -9,13 +9,13 @@ const fetchSuperHeroes = () => {
 }
 
 const RQSuperHeroes = () => {
-    const {isLoading, data, isError, error, isFetching} = useQuery("super-heroes", fetchSuperHeroes, {
-        refetchOnWindowFocus: true
+    const {isLoading, data, isError, error, isFetching, refetch} = useQuery("super-heroes", fetchSuperHeroes, {
+        enabled: false
     });
 
     console.log({ isLoading, isFetching });
 
-    if(isLoading) {
+    if(isLoading || isFetching) {
         return (
             <Spinner />
         )
@@ -29,6 +29,7 @@ const RQSuperHeroes = () => {
         <>
             <Navbar />
             <h2>RQ Super Heroes Page</h2>
+            <button onClick={refetch}> Fetch Button </button>
             {data?.data.map(hero => {
                 return (
                     <div key={hero.name}>{hero.name}</div>
